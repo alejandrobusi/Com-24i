21d5b624ab3d4cb9a55e2b0d0abb33573f6f6da3
Alejandro Busi <Alejandrobusi@gmail.com>
2022-10-24 21:25:39 -0300

    introduccion a js

---
+++ js/app.js
@@ -0,0 +1,82 @@
+// console.log( 1 + 3 );
+// // alert('Hola Comi24i');             
+
+// console.log('soy un console.log');
+
+// // Variables y tipos de datos!
+
+// var nombre;
+
+// let nombre2 = 'ale';
+
+// console.log(nombre2);
+
+// const num1 = '20';
+// const num2 = 21;
+// console.log(num1);
+
+// console.log( num1 + num2 );
+
+// const verdadero = true;
+// const falso = false;
+// console.log(verdadero);
+// console.log(falso);
+
+// let indefinido = undefined;
+// console.log(indefinido);
+
+// let sinRelleno = null;
+// console.log(sinRelleno)
+
+// // Operadores asignacion
+
+// let edad = 18;
+
+// // Operadores aritmeticos
+
+// console.log( 1 + 3 );
+
+// console.log( 1 - 3 );
+
+// console.log( 2 * 2 );
+
+// console.log( 2 / 2 );
+
+// // operadores unitarios
+
+// let incrementar = 1;
+// incrementar++;
+// console.log(incrementar);
+
+// let decrementar = 3;
+// decrementar--;
+// console.log(decrementar);
+
+// let bool = true;
+// console.log(!bool);
+
+// let bool2 = false;
+// console.log(!bool2);
+
+// operadores Relacionales
+
+// console.log( 1 == 12);
+
+// console.log( 1 != 1 );
+
+// let numero1 = 2;
+// let numero2 = '2';
+
+// console.log( numero1 === numero2 );
+
+// console.log( 2 >= 3 );
+
+// console.log( 2 <= 2 );
+
+// Operadores logicos
+
+console.log( 2 == 1 && 2 == 2 );
+
+console.log( !(1 == 2) || !(1 == 2) );
+
+console.log(!(1 == 2));
