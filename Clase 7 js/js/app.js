2bd61117b4d6a1ea0211018ac31ef4da0e039f64
Alejandro Busi <Alejandrobusi@gmail.com>
2022-11-14 23:58:03 -0300

    added class 7

---
+++ Clase 7 js/js/app.js
@@ -0,0 +1,45 @@
+// const alumnos = ['Ale', 'Yoha', 'Santi', 'Juli'];
+// const frutas = ['Manzana', 'Pera', 'Banana', 'Uvas'];
+
+// saludar(frutas);
+
+// function saludar (listOfFruits) {
+//   listOfFruits.map((student) => {
+//     console.log(`Hola, ${student}`);
+//   });
+
+//   for (let i = 0; i < listOfFruits.length; i++) {
+//     console.log(`Chau ${listOfFruits[i]}`);
+//   };
+// };
+
+
+// const filterLength = function (listOfFruits) {
+//   console.log('Hola');
+  
+// } ;
+
+// filterLength();
+
+// const saludar3 = (x) => console.log(x);
+
+// if (10 == 110) {
+//   console.log('Si es igual');
+// } else {
+//   console.log('No es igual');
+// };
+// const age = 10
+
+// const isOld = age >= 18 ? 'si es mayor' : 'no es mayor';
+// const isOld2 = age >= 18 && 'si es mayor';
+
+// console.log(isOld);
+
+// const name = 'Ale';
+// const saludar4 = () => {
+//   const name = 'Axel';
+//   return name
+// };
+// const nameFunction = saludar4();
+// console.log(nameFunction);
+
